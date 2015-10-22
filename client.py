a4d7a6c38a5ed658537753fa2f3158648eb6b666
Ana Real <anavm4_@hotmail.com>
2015-10-22 21:08:09 +0200

    Apartado 4 solucionado error

--- client.py
+++ client.py
@@ -14,18 +14,17 @@ SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 
 # Contenido que vamos a enviar
-linea = ''
-for i in sys.argv[3:]:
-    linea += i + ' '
+LINE = " ".join(sys.argv[3:])
+linea = LINE.split()
+LINE = linea[0].upper() + ' ' + 'sip:' + linea[1] + ' ' + 'SIP/2.0'
 
-LINE = linea
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 print("Enviando: " + LINE)
-my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+my_socket.send(bytes(LINE, 'utf-8') + b'\r\n\r\n')
 data = my_socket.recv(1024)
 
 print('Recibido -- ', data.decode('utf-8'))
