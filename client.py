a2dbb7330640385a0410eb3c71621e1fd24c28e5
Ana Real <anavm4_@hotmail.com>
2015-10-26 15:47:11 +0100

    PEP8 pasado

--- client.py
+++ client.py
@@ -9,9 +9,11 @@ import sys
 import time
 
 # Cliente UDP simple.
-# Dirección IP del servidor.
+
 if len(sys.argv) != 6:
     sys.exit('Usage: client.py ip puerto register sip_address expires_value')
+
+# Dirección IP del servidor.
 
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
@@ -20,17 +22,16 @@ PORT = int(sys.argv[2])
 LINE = " ".join(sys.argv[3:])
 linea = LINE.split()
 LINE = linea[0].upper() + ' ' + 'sip:' + linea[1] + ' ' + 'SIP/2.0' + '\r\n'
-# LINE = 'holaa'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-Expires = linea[2]
+EXPIRES = linea[2]
 
-print(LINE + 'Expires: ' + Expires + '\n')
-LINE = LINE + Expires
+print(LINE + 'Expires: ' + EXPIRES + '\r\n\r\n')
+LINE = LINE + EXPIRES
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
 
