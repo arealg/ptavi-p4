850f03937fb989e39f8d4ffc34a063b2249e6e2b
Ana Real <anavm4_@hotmail.com>
2015-11-02 10:38:04 +0100

    Modificado

--- client.py
+++ client.py
@@ -8,22 +8,18 @@ import socket
 import sys
 import time
 
-# Cliente UDP simple.
 
 if len(sys.argv) != 6:
     sys.exit('Usage: client.py ip puerto register sip_address expires_value')
 
-# Dirección IP del servidor.
 
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 
-# Contenido que vamos a enviar
 LINE = " ".join(sys.argv[3:])
 linea = LINE.split()
 LINE = linea[0].upper() + ' ' + 'sip:' + linea[1] + ' ' + 'SIP/2.0' + '\r\n'
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
@@ -36,8 +32,6 @@ my_socket.send(bytes(LINE, 'utf-8') + b'\r\n\r\n')
 data = my_socket.recv(1024)
 
 print(data.decode('utf-8'))
-print("Terminando socket...")
+print("Socket finalizado.")
 
-# Cerramos todo
 my_socket.close()
-print("Fin.")
